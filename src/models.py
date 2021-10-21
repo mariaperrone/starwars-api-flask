fee51f1c9e4938473a2a0d7d41191a70eaf5abdc
mariaperrone <perronemar148@gmail.com>
2021-10-21 00:24:58 +0000

    models finished

--- src/models.py
+++ src/models.py
@@ -1,12 +1,14 @@
+import enum 
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import enum
-
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password  = db.Column(db.String(50), unique=False, nullable=False)
+    fav_character = db.relationship('Fav_Character', backref='user')
+    fav_planet = db.relationship('Fav_Planet', backref='user')
 
 class GenderEnum(enum.Enum):
     male=1
@@ -20,7 +22,8 @@ class Character(db.Model):
     gender= db.Column(db.Enum(GenderEnum, unique=False, nullable=True))
     height= db.Column(db.Integer, unique=False, nullable=True)
     skin_color = db.Column(db.String(20), unique=False, nullable=True)
-    eye_color = Column(String(20), unique=False, nullable=True)
+    eye_color = db.Column(db.String(20), unique=False, nullable=True)
+    fav_character = db.relationship('Fav_Character', backref='character')
 
 class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -28,11 +31,20 @@ class Planet(db.Model):
     rotation_period = db.Column(db.Integer, unique=False, nullable=True)
     orbital_period = db.Column(db.Integer, unique=False, nullable=True)
     diameter = db.Column(db.Integer, unique=False, nullable=True)
-    climate = Column(db.String(20), unique=False, nullable=True)
-    gravity = Column(db.String(20), unique=False, nullable=True)
-    terrain = Column(db.String(20), unique=False, nullable=True)
+    climate = db.Column(db.String(20), unique=False, nullable=True)
+    gravity = db.Column(db.String(20), unique=False, nullable=True)
+    terrain = db.Column(db.String(20), unique=False, nullable=True)
     surface_water = db.Column(db.Integer, unique=False, nullable=True)
     population = db.Column(db.Integer, unique=False, nullable=False)
+    fav_planet = db.relationship('Fav_Planet', backref='planet')
+
+class Fav_Character(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'), primary_key=True)
+
+class Fav_Planet(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), primary_key=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
